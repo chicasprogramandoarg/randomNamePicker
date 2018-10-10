e4655116bf9f5dabe57a360fb0d6b0fa211cd598
Mar <marical2222@gmail.com>
2018-10-10 15:52:35 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,124 @@
+"use strict";
+
+// Change to false if you don't want a timer
+const showTimer = false;
+
+// Set timer countdown time here in minutes : seconds format
+const time = 0 + ":" + 19;
+
+// Add list of names here
+const namesList = [
+	'Constanza Beccan',
+	'Dony Cisneros',
+	'Ayelen María Guini', 
+	'Belén Rey',
+	'Catalina Syddall',
+	'Lucía Wainfeld',
+    'Mercedes Mena',
+    'Jessica Gomez',
+    'Leandro Vaiser',
+    'Aixa Halac',
+    'Boris Daniel Sortino',
+    'Sonia Morales',
+    'Julieta Juarez',
+    'Joaquín Vicente',
+    'Facu Saldaña',
+    'Matias Mendoza'
+];
+
+// Default variables
+let i = 0;
+let x = 0;
+let intervalHandle = null;
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const headerOne = document.getElementById('headerNames');
+// const timesUp = document.getElementById('timesUp');
+// const timerWrapper = document.getElementById('timerWrapper');
+// const timer = document.getElementById('timer');
+
+// Optional countdown timer
+// Add zero in front of numbers < 10
+// function checkSecond(sec) {
+//   if (sec < 10 && sec >= 0) {
+// 		sec = "0" + sec;
+// 	} 
+//   if (sec < 0) {
+// 		sec = "59";
+// 	}
+//   return sec;
+// }
+
+// const startTimer = function() {
+//   const presentTime = timer.innerHTML;
+//   const timeArray = presentTime.split(/[:]+/);
+//   let m = timeArray[0];
+//   let s = checkSecond((timeArray[1] - 1));
+	
+//   if (s==59) {
+// 		m = m-1;
+// 	}
+//   if (m < 0) {
+// 		timesUp.style.display = "block";
+// 	}
+  
+//   timer.innerHTML = m + ":" + s;
+	
+// 	setTimeout(startTimer, 1000);
+// }
+
+// Start or stop the name shuffle on button click
+startButton.addEventListener('click', function() {
+	this.style.display = "none";
+    stopButton.style.display = "block";
+	intervalHandle = setInterval(function () {
+        headerNames.textContent = namesList[i++ % namesList.length];
+    }, 50);
+	// if (showTimer===true) {
+	// 	timerWrapper.classList.remove('visible');
+	// }
+});
+stopButton.addEventListener('click', function() {
+	this.style.display = "none";
+    startButton.style.display = "block";
+	clearInterval(intervalHandle);
+	namesList.splice(namesList.indexOf(headerNames.textContent), 1)
+	console.log(namesList, headerNames.textContent)
+	timer.innerHTML = time;
+	if (showTimer===true) {
+		timerWrapper.classList.add('visible');
+	}
+	startTimer();
+	
+});
+
+// Allow use of spacebar to start/stop name shuffle
+document.body.onkeyup = function(e) {
+	if (e.keyCode == 32) {
+		if (x%2===0) {
+			startButton.style.display = "none";
+            stopButton.style.display = "block";
+			intervalHandle = setInterval(function () {
+                headerNames.textContent = namesList[i++ % namesList.length];
+			}, 50);
+			if (showTimer===true) {
+				timerWrapper.classList.remove('visible');
+			}
+		} else {
+			startButton.style.display = "block";
+			stopButton.style.display = "none";
+            clearInterval(intervalHandle);
+			timer.innerHTML = time;
+			if (showTimer===true) {
+				timerWrapper.classList.add('visible');
+			}
+			startTimer();
+		}
+		x++; 
+	}
+} 
+
+// Blinking warning
+// var backgroundInterval = setInterval(function() {
+//   timesUp.classList.toggle("backgroundRed");
+// }, 1000)
